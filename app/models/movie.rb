8a60ddc35141f3b54400f573bc03c00583442f67
Ubuntu <ubuntu@ip-172-31-50-40.ec2.internal>
2022-02-27 09:26:45 +0000

    First submit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
+# connect to the database table.
 class Movie < ActiveRecord::Base
+    
+    ## get the rating of the Movie.
+    def Movie.all_rating
+        Movie.all.map{|x| x.rating}.uniq
+    end
 end
